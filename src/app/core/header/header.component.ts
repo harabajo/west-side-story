6bdc12a0d1c8fe8fed7b35253fa17d7383f170d4
harabajo <ranmerari@gmail.com>
2022-12-09 11:53:07 +0200

    start header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  currentRoute!: string;
+
+  constructor(private router: Router,) { }
+
+  ngOnInit() {
+    this.router.events.pipe(filter((event: any) => event instanceof NavigationEnd)).subscribe((event: { url: string; }) => {
+      this.currentRoute = event.url;
+    });
+  }
 }
+
